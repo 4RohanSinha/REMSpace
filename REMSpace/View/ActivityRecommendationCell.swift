1d552c92fab3f35bbe71764524cb1e3f86b4c957
4RohanSinha <rohansinha1@yahoo.com>
2023-04-03 15:11:09 -0700

    Latest changes 4/3

--- REMSpace/View/ActivityRecommendationCell.swift
+++ REMSpace/View/ActivityRecommendationCell.swift
@@ -11,7 +11,6 @@ class ActivityRecommendationCell: UITableViewCell {
 
     @IBOutlet weak var activityName: UITextField!
     @IBOutlet weak var activityIsDoneSwitch: UISwitch!
-    @IBOutlet weak var activityDescr: UITextView!
     
     func configure(withActivity activity: Activity) {
         activityName.text = activity.name
